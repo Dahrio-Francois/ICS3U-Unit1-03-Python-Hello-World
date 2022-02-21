81913ce30185c99738db522a9844985d9c2b165c
Dahrio-Francois <dahrio.francois@mths.ca>
2022-02-21 21:20:00 +0000

    added hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
-#1/usr/bin/env python3
+#!/usr/bin/env python3
 
 # Created by: Dahrio Francois
 # Created on: February 2022
@@ -7,7 +7,7 @@
 
 def main():
     print("Hello, World!")
-    
-    
+
+
 if __name__ == "__main__":
     main()
